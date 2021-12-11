21b5236505cdf08c6b1a7227249f4f36a0b7700f
deployKey <drnakkoc94@gmail.com>
2021-12-11 12:19:19 +0000

    create getItems function

--- lamdas/endpoints/deleteItem.js
+++ lamdas/endpoints/deleteItem.js
@@ -2,31 +2,17 @@ const Responses = require('../common/API_Responses')
 const AWS = require('aws-sdk')
 const documentClient = new AWS.DynamoDB.DocumentClient();
 const TableName = process.env.tableName
-
 exports.handler = async event => {
     if(!event.pathParameters || !event.pathParameters.ID){
         Responses._400({message: 'ID or parameters not found'})
     }
 
-    const newItem = JSON.parse(event.body)
+    let ID = event.pathParameters.ID
 
-    const benefits = newItem.benefits.map(benefit => benefit)
     const params = {
         TableName,
-        Item: {
-            ID: newItem.ID,
-            benefits: benefits,
-            city: newItem.city,
-            companyType: newItem.companyType,
-            experience: newItem.experience,
-            extraSupport: newItem.extraSupport,
-            happyRate: newItem.happyRate,
-            numberOfWorkers: newItem.numberOfWorkers,
-            remoteAllowed: newItem.remoteAllowed,
-            salary: newItem.salary,
-            thingsYouKnow: newItem.thingsYouKnow,
-            thinksToChangeJob: newItem.thinksToChangeJob,
-            title: newItem.title,
+        Key: {
+            ID: parseInt(ID)
         }
     }
 
